0e72598dd2125a746e6e5117f1a31ca9651677d9
pcodesdev <njugunapm23@gmail.com>
2023-08-14 22:23:09 +0300

    Initial commit

---
+++ src/main/kotlin/excercises.kt
@@ -0,0 +1,38 @@
+//01
+
+//fun main(){
+//    println("I'm")
+//    println("learning")
+//    println("Kotlin!")
+//}
+
+//02
+//fun main() {
+//    println("Monday")
+//    println("Tuesday")
+//    println("Wednesday")
+//    println("Thursday")
+//    println("Friday")
+//}
+
+//03
+//fun main() {
+//    println("Tomorrow is rainy")
+//}
+
+//04
+//fun main() {
+//    println("There is a chance of snow")
+//}
+
+//05
+//fun main() {
+//    println("Cloudy")
+//    println("Partly Cloudy")
+//    println("Windy")
+//}
+
+//06
+fun main() {
+    println("How's the weather today?")
+}
